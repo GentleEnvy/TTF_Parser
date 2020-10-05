383cda318e9aa2587d646c3e086b5b404733c6ab
Envy <envy15@mail.ru>
2020-10-05 20:03:28 +0300

    Initial commit

---
+++ src/Task_3/Main.java
@@ -0,0 +1,10 @@
+package Task_3;
+
+import Task_3.Window.Window;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Window.startWindow();
+    }
+}
---
+++ src/Task_3/Window/Window.java
@@ -0,0 +1,28 @@
+package Task_3.Window;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+
+public class Window extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader mainLoader = new FXMLLoader(
+                getClass().getResource("main.fxml")
+        );
+        Parent root = mainLoader.load();
+
+
+        stage.setMaximized(true);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void startWindow() {
+        launch();
+    }
+}
